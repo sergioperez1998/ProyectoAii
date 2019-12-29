65c64483d03a159be3a3d20cf6985907ae6bec3b
sergioperez1998 <sergio-pm@hotmail.com>
2019-12-29 18:01:27 +0100

    Scraping datos de los juegos de nintendo switch terminado

--- JSGames/ScrapingWeb/datosJuegos.py
+++ JSGames/ScrapingWeb/datosJuegos.py
@@ -189,7 +189,7 @@ def datosJuegosNintendoSwitch():
 
     urlBasica="https://www.eneba.com"
 
-    #urlJuegosNintendoSwitch="https://www.eneba.com/es/store?page=1&platforms[]=NINTENDO&types[]=game"
+    urlJuegosNintendoSwitch="https://www.eneba.com/es/store?page=1&platforms[]=NINTENDO&types[]=game"
     #urlJuegosPS4 = "https://www.eneba.com/es/store/psn-games"
 
     month = {	'Janauary':'01',
@@ -204,3 +204,102 @@ def datosJuegosNintendoSwitch():
 		'October':'10',
 		'November':'11',
 		'December':'12'		}
+
+    reqSwitch = Request(urlJuegosNintendoSwitch, headers={'User-Agent': 'Mozilla/5.0'})
+    soupSwitch = BeautifulSoup(urlopen(reqSwitch).read().decode("latin-1"), 'html.parser')
+
+    listadoNombresJuegosSwitch=[]
+    listadoImagenesJuegosSwitch=[]
+    listadoUrlJuegosSwitch=[]
+    listadoPreciosJuegosSwitchActual=[]
+    listadoGenerosJuegoSwitchTotal=[]
+    listadoGenerosPagina=[]
+    listadoFechaLanzamientoJuegoSwitch=[]
+
+    for juegos in soupSwitch.find_all("div", attrs={"class":"_3M7T08"}):
+        for juego in juegos.find_all("div", attrs={"class":"_2rxjGA"}):
+            for datos1 in juego.find_all("div", attrs={"class":"_3shANq"}):
+                for nombre in datos1.find_all("div", attrs={"class":"_1ZwRcm"}):
+                    nombreJuego=nombre.span.get_text()
+                    nombreJuegoSerializado= eliminadorDiacriticos(nombreJuego)
+                    listadoNombresJuegosSwitch.append(nombreJuegoSerializado)
+                for img in datos1.find_all("div", attrs={"class":"_2vZ2Ja _1p1I8b"}):
+                    imagenJuego=img.img.get("src")
+                    imagenJuegoSerializada = eliminadorDiacriticos(imagenJuego)
+                    listadoImagenesJuegosSwitch.append(imagenJuegoSerializada)
+            for datos2 in juego.find_all("div", attrs={"class":"_12ISZC"}):
+                for url in datos2.find_all("a", attrs={"class":"_2idjXd"}):
+                    urlJuego=urlBasica + url.get("href")
+                    listadoUrlJuegosSwitch.append(urlJuego)
+
+    for juegoSwitch in listadoUrlJuegosSwitch:
+
+        reqJuegoSwitch = Request(juegoSwitch, headers={'User-Agent': 'Mozilla/5.0'})
+        soupJuegoSwitch = BeautifulSoup(urlopen(reqJuegoSwitch).read().decode("latin-1"), 'html.parser')
+
+        precioJuegoAhora= soupJuegoSwitch.find("span", attrs={"class":"_1fTsyE"})
+        precioJuegoSwitchAhora = precioJuegoAhora.get_text().split("¬")[1]
+        listadoPreciosJuegosSwitchActual.append(precioJuegoSwitchAhora)
+
+        for generos in soupJuegoSwitch.find_all("ul", attrs={"class":"_3w9_g5"}):
+            listadoGenerosJuegoSwitch=[]
+            for generosJuegos in generos.find_all("li"):
+                genero=generosJuegos.a.get_text()
+                generoSerializado=eliminadorDiacriticos(genero)
+                if "A³" in generoSerializado:
+                    generoSerializado="Accion"
+                listadoGenerosJuegoSwitch.append(generoSerializado)
+                if not(generoSerializado in listadoGenerosPagina):
+                    listadoGenerosPagina.append(generoSerializado)
+            listadoGenerosJuegoSwitchTotal.append(listadoGenerosJuegoSwitch)
+        
+        fechaLanzamiento = soupJuegoSwitch.find("p", attrs={"class":"FpVQmt"})
+        fechaLanzamientoJuegoSwitch=(fechaLanzamiento.get_text().split(" ")[1].strip(",")+"-"
+            +month[fechaLanzamiento.get_text().split(" ")[0]]+"-"+fechaLanzamiento.get_text().split(" ")[2])
+        listadoFechaLanzamientoJuegoSwitch.append(fechaLanzamientoJuegoSwitch)
+
+    contenidoJuegosSwitch = {
+        "Nombres":listadoNombresJuegosSwitch,
+        "Precios":listadoPreciosJuegosSwitchActual,
+        "Plataforma":"Nintendo Switch",
+        "Generos":listadoGenerosJuegoSwitchTotal,
+        "Url":listadoUrlJuegosSwitch,
+        "Imagenes":listadoImagenesJuegosSwitch,
+        "FechaLanzamiento":listadoFechaLanzamientoJuegoSwitch,
+        "GenerosPagina":listadoGenerosPagina
+    }
+
+    return contenidoJuegosSwitch
+
+def datosJuegosPs4():
+
+    urlBasica="https://www.eneba.com"
+
+    urlJuegosPS4 = "https://www.eneba.com/es/store/psn-games"
+
+    month = {	'Janauary':'01',
+		'February':'02',
+		'March':'03',
+		'April':'04',
+		'May':'05',
+		'June':'06',
+		'July':'07',
+		'August':'08',
+		'September':'09',
+		'October':'10',
+		'November':'11',
+		'December':'12'		}
+
+    reqPs4 = Request(urlJuegosPS4, headers={'User-Agent': 'Mozilla/5.0'})
+    soupPs4 = BeautifulSoup(urlopen(reqPs4).read().decode("latin-1"), 'html.parser')
+
+    listadoNombresJuegosPs4=[]
+    listadoImagenesJuegosPs4=[]
+    listadoUrlJuegosPs4=[]
+    listadoPreciosJuegosSwitchPs4=[]
+    listadoGenerosJuegoPs4Total=[]
+    listadoGenerosPagina=[]
+    listadoFechaLanzamientoJuegoPs4=[]
+
+
+datosJuegosPs4()
